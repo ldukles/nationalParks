74ec88c37f8ba0be5ab416c76abb416a0b462638
[Lindsey Dukles] <Lindseydukles@gmail.com>
2022-09-19 15:27:10 -0400

    readmeupdates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [,] = useState({});
+
+  const handleSubmit = async () => {
+    const response = await fetch()
+
+    // parse JSON response into a javascript object
+    const data = await response.json();
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 

79143a4c552c2bf76478833ff2417268713db02f
[Lindsey Dukles] <Lindseydukles@gmail.com>
2022-10-21 12:31:34 -0400

    api call

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState } from "react";
-import './App.css';
+import './App.css'; 
+
+
 
 function App() {
-  const [,] = useState({});
+  const [parksData, setParksData] = useState({});
 
   const handleSubmit = async () => {
-    const response = await fetch()
+    const response = await fetch('https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=K54Ux2w79vRCRO9fWdxuMxN38EzPvYTPb43Lq9MC')
 
     // parse JSON response into a javascript object
     const data = await response.json();
+    setParksData(data)
   }
+  console.log(parksData)
   return (
-    
+    <div>
+    <button onClick={handleSubmit}>Submit</button>
+    <h2>{data.fullname}</h2>
+    </div>
   );
 }
 
 export default App;
+
+// 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=K54Ux2w79vRCRO9fWdxuMxN38EzPvYTPb43Lq9MC'
+
+// KEY K54Ux2w79vRCRO9fWdxuMxN38EzPvYTPb43Lq9MC
